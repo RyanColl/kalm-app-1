6be05e34561568e02e6e345674121de07975b0a4
Ryan Collicutt <ryancollicutt22@gmail.com>
2022-01-25 15:45:36 -0800

    make sure credentials are removed before public :D

--- aws/credentials/credentials.js
+++
@@ -1,9 +0,0 @@
-
-
-const creds = {
-    region: 'us-west-2',
-    bucketName: 'kalms3',
-    accessKeyId: 'AKIAUHB6WHCNLOEBYL6U',
-    secret: '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',
-}
-module.exports = {creds}
--- aws/credentials/twilio.creds.js
+++
@@ -1,5 +0,0 @@
-
-
-const ssid = 'ACc65000771253cf62027c9f3d798558d4'
-const secret = '25ff86f260e375dafc288791ef3cceb8'
-module.exports = {ssid, secret}
